02a78eeaf5cc2103c6f144dceaeeab983c5bb3b4
Bingzan <lbzlillian@gmail.com>
2014-04-03 10:53:23 +1100

    calculate length normalised TF-IDF

--- termDoc.m
+++ termDoc.m
@@ -2,7 +2,7 @@ function [ mTermDoc ] = termDoc( freqMatrix )
 %termDoc Create the term-document matrix according to the frequent matrix
 %
 % @author: Bingzan Liang
-% Last update: 28/03/2014
+% Last update: 02/04/2014
 
     mTermDoc = zeros(max(freqMatrix(:,2)),max(freqMatrix(:,1))); %rows - num of docs; columns - num of terms
 
@@ -14,9 +14,20 @@ function [ mTermDoc ] = termDoc( freqMatrix )
         mTermDoc(:,i) = log(mTermDoc(:,i)+1) * log(max(freqMatrix(:,2)) / size(find(freqMatrix(:,1) == i),1)); %calculate the TF_IDF score
     end 
     
-    filter = findMostFreq( freqMatrix,3 ); 
+    for i=1:size(mTermDoc,1)
+        sumDoc = 0;
+        for j=1:size(mTermDoc,2)
+            sumDoc = sumDoc + mTermDoc(i,j)^2;
+        end
+        
+        len = sqrt(sumDoc);
+        
+        mTermDoc(i,:) = mTermDoc(i,:) / len;        
+    end
     
-    mTermDoc = mTermDoc(:,(filter));
+    %filter = findMostFreq( freqMatrix,3 );
+    
+    %mTermDoc = mTermDoc(:,(filter));
     mTermDoc = -mTermDoc;
 
    
